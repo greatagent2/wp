73cd4c838a8f752ec0eb9d03c3ab3950b52930f2
wwqgtxx <wwqgtxx@gmail.com>
2013-08-27 16:33:41 +0800

    bat

---
+++ autoupdate.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding:utf-8
+# autoupdate.py
+# Author: Wang Wei Qiang <wwqgtxx@gmail.com>
+
+
+import sys
+import os
+import glob
+
+sys.path += glob.glob('%s/*.egg' % os.path.dirname(os.path.abspath(__file__)))
+
+try:
+	if 'threading' in sys.modules:
+		del sys.modules['threading']
+	import gevent
+	import gevent.socket
+	import gevent.monkey
+	gevent.monkey.patch_all()
+except (ImportError, SystemError):
+	gevent = None
+try:
+	import OpenSSL
+except ImportError:
+	OpenSSL = None
+
+from simpleproxy import LocalProxyServer
+from simpleproxy import server
+from simpleproxy import logging
+from simpleproxy import common as proxyconfig
+from common import sysconfig as common
+from common import FileUtil
+from common import Config
+from common import config
+from common import __config__
+from common import __sha1__
+from common import __sign__
+from common import __file__
+from common import __version__
+from common import __versionfile__
+from makehash import makehash
+from sign import verify
+from sign import sign
+
+import os
+import sys
+import re
+import ConfigParser
+import hashlib
+import thread
+import urllib2
+import random
+
+
+
+class Updater(object):
+	def __init__(self,serverurl,old_file_sha1_ini,dir):
+		proxies = {'http':'%s:%s'%('127.0.0.1', proxyconfig.LISTEN_PORT),'https':'%s:%s'%('127.0.0.1', proxyconfig.LISTEN_PORT)}
+		self.opener = urllib2.build_opener(urllib2.ProxyHandler(proxies))
+		self.server = str(serverurl)
+		self.old_file_sha1_ini = old_file_sha1_ini
+		self.dir = dir
+	def netopen(self,filename):
+		print 'Getting	'+filename
+		file = self.opener.open(self.server+filename).read()
+		print 'Get	'+filename+'				OK!'
+		return file
+	def getfile(self,filename):
+		while 1:
+			try:
+				file = self.netopen(filename)
+				return file
+			except Exception as e:
+				print e
+				return
+	def writefile(self,filename,sha1v):
+		file = self.getfile(filename)
+		path = self.dir+filename
+		if os.path.isfile(path):
+			input = FileUtil.open(path,"r")
+			oldfile = input.read()
+			input.close()
+		else:
+			oldfile = None
+		output = FileUtil.open(path,"wb")
+		output.write(file)
+		print 'Update	'+filename+'				OK!'
+		output.close()
+		input = FileUtil.open(path,"rb")
+		sha1vv = FileUtil.get_file_sha1(input)
+		#print sha1v.strip()
+		#print sha1vv.strip()
+		input.close()
+		if sha1v.strip()==sha1vv.strip() :
+			print 'Verify	'+filename+'				OK!'
+		else:
+			print 'Verify	'+filename+'				Fail!'
+			if oldfile:
+				output = FileUtil.open(path,"wb")
+				output.write(oldfile)
+				output.close()
+			print 'Recover	'+filename+'				OK!'
+			
+	def getnewsha1(self,path,oldsha1):
+		output = FileUtil.open(path,"wb")
+		output.write(self.netopen('/'+__sha1__)) 
+		output.close()
+		input = FileUtil.open(path,"r")
+		tmp2 = input.read()
+		input.close()
+		hash = self.netopen('/'+__sign__)
+		print 'Verifing Hash Table.....'
+		ok = verify(tmp2,hash)
+		if not ok:
+			print 'Verify Failed!'
+			sys.exit()
+		print 'Verify Successful1!'
+
+	def update(self):
+		versionfile = self.netopen('/'+__versionfile__)
+		print "Show Server Version Message:"
+		print versionfile
+		oldsha1 = self.old_file_sha1_ini
+		path = __sha1__+'.tmp'
+		FileUtil.if_has_file_remove(path)
+		self.getnewsha1(path,oldsha1)
+		newsha1 = Config(path)
+		for path, sha1v in newsha1.getsection('FILE_SHA1'):
+			if not (sha1v == oldsha1.getconfig('FILE_SHA1',path)):
+				path = path.replace('$path$','')
+				path = path.replace('\\','/')
+				self.writefile(path,sha1v)
+		FileUtil.if_has_file_remove(path)
+
+
+
+def main():
+	dir = FileUtil.cur_file_dir()
+	os.chdir(dir)
+	sys.stdout.write(common.info())
+	sys.stdout.write(proxyconfig.info())
+	thread.start_new_thread(server.serve_forever, tuple())
+	sha1 = makehash(dir)
+	updater = Updater(common.AUTOUPDATE_SERVER[0],sha1,dir)
+	updater.update()
+
+	#for path, sha1v in sha1.getsection('FILE_SHA1'):
+		#newpath = path.replace('$path$',dir)
+		#print newpath + ' = ' + sha1v
+
+if __name__ == '__main__':
+	main()
---
+++ common.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding:utf-8
+# common.py
+# Author: Wang Wei Qiang <wwqgtxx@gmail.com>
+
+__config__   = 'autoupdate.ini'
+__sha1__   = 'sha1.ini'
+__sign__   = 'sha1.sign'
+__git__   = 'git.txt'
+__versionfile__ = 'version'
+__pubkey__   = './data/greatagent2.pubkey'
+__prikey__   = '../greatagent2.prikey'
+__author__   = 'Wang Wei Qiang <wwqgtxx@gmail.com>'
+__names__   = 'GreatAgent2-GA'
+__version__ = '2.0.0'
+__file__	 = 'autoupdate.py'
+
+import sys
+import os
+import glob
+
+sys.path += glob.glob('%s/*.egg' % os.path.dirname(os.path.abspath(__file__)))
+sys.path += glob.glob('%s/lib/*.egg' % os.path.dirname(os.path.abspath(__file__)))
+
+try:
+	if 'threading' in sys.modules:
+		del sys.modules['threading']
+	import gevent
+	import gevent.socket
+	import gevent.monkey
+	gevent.monkey.patch_all()
+except (ImportError, SystemError):
+	gevent = None
+try:
+	import OpenSSL
+except ImportError:
+	OpenSSL = None
+
+import re
+import ConfigParser
+import hashlib
+import random
+
+import rsa,base92,pyasn1
+
+class FileUtil(object):
+	@staticmethod
+	def getfile(filename):
+		global __file__
+		__file__ = os.path.abspath(__file__)
+		if os.path.islink(__file__):
+			__file__ = getattr(os, 'readlink', lambda x:x)(__file__)
+		os.chdir(os.path.dirname(os.path.abspath(__file__)))
+		return os.path.join(os.path.dirname(__file__), filename)
+
+	@staticmethod
+	def if_has_file_remove(filename):
+		if os.path.isfile(FileUtil.getfile(filename)):
+			os.remove(FileUtil.getfile(filename)) 
+
+	@staticmethod
+	def get_file_sha1(f):
+		m = hashlib.sha1()
+		while True:
+			data = f.read(10240)
+			if not data:
+				break
+			m.update(data)
+		return m.hexdigest()
+		
+	@staticmethod
+	def get_data_sha1(data):
+		m = hashlib.sha1(data)
+		return m.hexdigest()
+
+	@staticmethod
+	def sumfile(fpath):
+		input = FileUtil.open(fpath,'rb')
+		sum = FileUtil.get_file_sha1(input)
+		input.close()
+		return sum
+
+	@staticmethod
+	def cur_file_dir():
+		path = sys.path[0]
+		if os.path.isdir(path):
+			return path
+		elif os.path.isfile(path):
+			return os.path.dirname(path)
+
+	@staticmethod
+	def open(path,type):
+		if type.endswith('w') or type.startswith('w'):
+			if path.endswith(sysconfig.REGEX_ONLYW)or type.endswith('b'):
+				return open(path,type)
+			else:
+				return open(path,type+"b")
+		elif type.endswith('r') or type.startswith('r'):
+			if path.endswith(sysconfig.REGEX_ONLYR)or type.endswith('b'):
+				return open(path,type)
+			else:
+				return open(path,type+"b")
+		else:
+			return
+
+
+
+class Config(object):
+
+	def __init__(self,config):
+		"""load config from proxy.ini"""
+		ConfigParser.RawConfigParser.OPTCRE = re.compile(r'(?P<option>[^=\s][^=]*)\s*(?P<vi>[=])\s*(?P<value>.*)$')
+		self.FILENAME = config
+		self.CONFIG = ConfigParser.ConfigParser()
+		self.CONFIG.read(FileUtil.getfile(config))
+
+	def writeconfig(self,section, option,str):
+		if not self.CONFIG.has_section(section):
+			self.CONFIG.add_section(section)
+		self.CONFIG.set(section,option,str)
+		f = FileUtil.open(FileUtil.getfile(self.FILENAME),'w') 
+		self.CONFIG.write(f)
+		f.close()
+	
+	def getconfig(self,section, option):
+		return self.CONFIG.get(section, option)if self.CONFIG.has_option(section, option) else ''
+
+	def getsection(self,section):
+		return self.CONFIG.items(section) if self.CONFIG.has_section(section) else ''
+
+config = Config(__config__)
+
+class Common(object):
+	"""Global Config Object"""
+
+	def __init__(self):
+		"""load config from ini"""
+		self.CONFIG = config.CONFIG
+		
+		self.AUTOUPDATE_SERVER = self.CONFIG.get('autoupdate', self.CONFIG.get('autoupdate', 'server')).split('|')
+		self.REGEX_START = tuple(x for x in self.CONFIG.get('regex', 'start').split('|') if x)
+		self.REGEX_END = tuple(x for x in self.CONFIG.get('regex', 'end').split('|') if x)
+		self.REGEX_ONLYW = tuple(x for x in self.CONFIG.get('regex', 'onlyw').split('|') if x)
+		self.REGEX_ONLYR = tuple(x for x in self.CONFIG.get('regex', 'onlyr').split('|') if x)
+		
+		random.shuffle(self.AUTOUPDATE_SERVER)
+
+		
+	def info(self):
+		info = ''
+		info += '------------------------------------------------------\n'
+		info += 'GreatAgent Version	: %s \n'% (__version__)
+		info += 'RunTime            :python/%s %spyopenssl/%s rsa/%s PyASN1/%s base92/%s\n' % (sys.version[:5], gevent and 'gevent/%s ' % gevent.__version__ or '', getattr(OpenSSL, '__version__', 'Disabled'),rsa.__version__,pyasn1.__version__,base92.__version__)
+		info += 'Update Server      : %s\n' % '|'.join(self.AUTOUPDATE_SERVER)
+		info += '------------------------------------------------------\n'
+		return info
+
+
+sysconfig = Common()
